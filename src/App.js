1f79ee91588b2e13e7e7e39bba5acc92f29cb748
garrett-vangilder <garrett.vangilder@gmail.com>
2022-06-20 17:12:22 -0500

    Adds react-navigation deps

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import {SafeAreaView} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 import {Home} from './views';
 import {styles} from './constants';
 
 const App = () => (
-  <SafeAreaView style={{backgroundColor: styles.colors.white.one}}>
-    <Home />
-  </SafeAreaView>
+  <NavigationContainer>
+    <SafeAreaView style={{backgroundColor: styles.colors.white.one}}>
+      <Home />
+    </SafeAreaView>
+  </NavigationContainer>
 );
 export default App;
